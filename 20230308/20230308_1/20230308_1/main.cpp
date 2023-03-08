58fdf72b0fc37103e0f4c96bdbfcb012a2c54390
ikkf2 <127211215+ikkf2@users.noreply.github.com>
2023-03-08 10:03:54 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>  //header 파일 추가 standard IO
+
+
+int main(void)
+{
+
+	printf("test \n");  // \n line reed
+	printf("202315018 \n");  // \n line reed
+	printf("CHOI Yun Seok \n");  // \n line reed
+	return 0;
+}
+
+// ctrl+F5 는 실행
