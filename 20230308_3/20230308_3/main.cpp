52dffdfcb54ebe29224fed8969a48c5b0224e147
ikkf2 <127211215+ikkf2@users.noreply.github.com>
2023-03-08 13:03:52 +0900

    Add files via upload

---
+++ 20230308_3/20230308_3/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h> //header 파일 추가 Standard IO
+
+
+int main(void)
+{
+	printf(" 1 x 1 = 1\n");
+	printf(" 1 x 2 = 2\n");
+	printf(" 1 x 3 = 3\n");
+	printf(" 1 x 4 = 4\n");
+	printf(" 1 x 5 = 5\n");
+	printf(" 1 x 6 = 6\n");
+	printf(" 1 x 7 = 7\n");
+	printf(" 1 x 8 = 8\n");
+	printf(" 1 x 9 = 9\n");
+	
+	return 0;
+}
